5ef56c869da9679020a38933dab323334fbbed5c
vladstpnv54 <vladstpnv54@mail.ru>
2019-05-07 02:49:36 +0700

    Add board.h

---
+++ src/board.h
@@ -0,0 +1,13 @@
+#ifndef BOARD_H
+#define BOARD_H
+
+void scanan();
+int chartoint(char input[6]);
+int white();
+int black();
+void move();
+int checkY();
+int checkWIn(int status);
+void transformPawn();
+
+#endif
