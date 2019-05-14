c35991bc8209a41eb9c660c2e3a6558f8232cd38
vladstpnv54 <vladstpnv54@mail.ru>
2019-05-14 12:40:10 +0700

    Fix bugs

--- src/board_print_plain.c
+++ src/board_print_plain.c
@@ -3,17 +3,18 @@
 
 extern char board[8][8];
 
-void printdesk() {
-  int i, j;
-  for (i = 7; i >= 0; i--) {
-    printf("%d ", i + 1);
-    for (j = 0; j < 8; j++) {
-      printf("%c ", board[i][j]);
+void printdesk()
+{
+    int i, j;
+    for (i = 7; i >= 0; i--) {
+        printf("%d ", i + 1);
+        for (j = 0; j < 8; j++) {
+            printf("%c ", board[i][j]);
+        }
+        printf("\n");
     }
+    printf("  ");
+    for (i = 0; i < 8; i++)
+    printf("%c ", i + 97);
     printf("\n");
-  }
-  printf("  ");
-  for (i = 0; i < 8; i++)
-  printf("%c ", i + 97);
-  printf("\n");
 }
--- src/main.c
+++ src/main.c
@@ -4,45 +4,47 @@
 
 char numbers[7];
 int X1, X2, Y1, Y2;
-char board[8][8] = {{'R', 'N', 'B', 'K', 'Q', 'B', 'N', 'R'},
-                    {'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
-                    {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-                    {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-                    {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-                    {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-                    {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
-                    {'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'}};
+char board[8][8] = {
+    {'R', 'N', 'B', 'K', 'Q', 'B', 'N', 'R'},
+    {'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
+    {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+    {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+    {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+    {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+    {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
+    {'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'}};
 
-int main() {
-  int status = 0;
+int main()
+{
+    int status = 0;
 
-  printf("     Шахматы\n");
-  printf("Пример хода: E2-E4\n");
-  printf("Пример взятия: E2xE4\n");
+    printf("     Шахматы\n");
+    printf("Пример хода: E2-E4\n");
+    printf("Пример взятия: E2xE4\n");
 
   printdesk();
 
-  while (1) {
-    printf("Белые (Большие буквы):");
-    scanan(1);
-    move();
-    printdesk();
-    status = checkWIn(1);
-    if (status != 0) {
-      break;
+    while (1) {
+        printf("Белые (Большие буквы):");
+        scanan(1);
+        move();
+        printdesk();
+        status = checkWIn(1);
+        if (status != 0) {
+            break;
+        }
+
+        printf("Черные (Маленькие буквы):");
+        scanan(2);
+        move();
+        printdesk();
+        status = checkWIn(2);
+        if (status != 0) {
+            break;
+        }
     }
 
-    printf("Черные (Маленькие буквы):");
-    scanan(2);
-    move();
-    printdesk();
-    status = checkWIn(2);
-    if (status != 0) {
-      break;
-    }
-  }
+    printf("Player %d win\n", status);
 
-  printf("Player %d win\n", status);
-
-  return 0;
+    return 0;
 }
